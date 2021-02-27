aa23f2d4866b66b455aac65b1cd1a93008e55efb
Jean-Marc Meessen <jean-marc@meessen-web.org>
2021-02-27 16:32:54 +0100

    Correct the output (Correct external IP & DNS)

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,7 +1,7 @@
 output "bastion_ip" {
-  value = aws_instance.bastion.public_ip
+  value = aws_eip.bastion.public_ip
 }
 
 output "bastion_dns" {
-  value = aws_instance.bastion.public_dns
+  value = aws_eip.bastion.public_dns
 }
