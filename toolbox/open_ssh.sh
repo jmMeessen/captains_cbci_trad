b661166f0b1300e8d638355eca4b9f00d2b683b7
Jean-Marc Meessen <jean-marc@meessen-web.org>
2021-02-28 18:29:19 +0100

    automate jumping to host via jump host

--- toolbox/open_ssh.sh
+++ toolbox/open_ssh.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# All nodes require a jump via the bastion
+needs_jump="yes"
+bastion_ip=$(terraform output -raw -state=../terraform/terraform.tfstate bastion_ip 2>&1)
+
 if [ $# -lt 1 ]
 then
         echo "Usage : $0 nodeName"
@@ -10,20 +14,29 @@ case "$1" in
 
 bastion)  
     terraform_name="bastion_ip"
+    needs_jump="no"
     ;;
 
+jenkins)
+    terraform_name="jenkins_ip"
+    ;;
 
-*) echo "nodeName $1 is not supported"
-   echo "supported node names are: bastion"
-   exit
-   ;;
+*) 
+    echo "nodeName $1 is not supported"
+    echo "supported node names are: bastion"
+    exit
+    ;;
 esac
 
 echo "Starting SSH on $1"
 #echo "$terraform_name"
 
 node_ip=$(terraform output -raw -state=../terraform/terraform.tfstate $terraform_name 2>&1)
-echo "${terraform_name} (${node_ip})"
-ssh ${node_ip} -l ubuntu -i ~/.ssh/captains_cbci_trad
 
-#ssh -J ubuntu@34.225.7.116 -i ~/.ssh/captains_cbci_trad  ubuntu@10.0.1.100  -i ~/.ssh/captains_cbci_trad
+if [ "$needs_jump" == "yes" ]; then
+  echo "${terraform_name} (${node_ip}) via bastion (${bastion_ip})"
+  ssh -J ubuntu@${bastion_ip} -i ~/.ssh/captains_cbci_trad  ubuntu@${node_ip} -i ~/.ssh/captains_cbci_trad
+else
+  echo "${terraform_name} (${node_ip})"
+  ssh ${node_ip} -l ubuntu -i ~/.ssh/captains_cbci_trad
+fi
