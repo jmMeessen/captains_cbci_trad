430f43bd06ff3a7bd49b505281804683925ac0f2
Jean-Marc Meessen <jean-marc@meessen-web.org>
2021-03-21 20:30:51 +0100

    Add TFSEC checks

---
+++ toolbox/infra_check_scy.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -e              #fail script when an instruction fails
+set -o pipefail     #fails script a piped instruction fails
+set -u              #fail script when a variable is uninitialised
+
+# For consistant processing, CD to the Toolbox directory
+cd "$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null && pwd)" || return
+
+# cd ../terraform
+tfsec ../terraform/ --out ../notes/tf_sec.json --format json
