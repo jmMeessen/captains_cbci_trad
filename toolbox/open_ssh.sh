ea59b88e85e31d87645fb5378304cae32a07863a
Jean-Marc Meessen <jean-marc@meessen-web.org>
2021-04-05 22:45:12 +0200

    Finalize terraform mods

--- toolbox/open_ssh.sh
+++ toolbox/open_ssh.sh
@@ -7,40 +7,57 @@ set -u              #fail script when a variable is uninitialised
 # For consistant processing, CD to the Toolbox directory
 cd "$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null && pwd)" || return
 
-
-PS3='Please enter your choice: '
-options=("bastion" "cjoc" "cm1" "agent_1" "docker_agent" "Quit")
-select opt in "${options[@]}"
-do
-    case $opt in
-        "bastion")
-            echo "Connecting to ${opt}"
-            ssh -F ../work_data/ssh_config ${opt}
+if [ $# -eq 1 ]
+then
+    param=$1
+    case $param in
+        bastion|cjoc|cm1|agent_1|docker_agent)
+            echo "Connecting to ${param}"
+            ssh -F ../work_data/ssh_config ${param}
             break
             ;;
-        "cjoc")
-            echo "Connecting to ${opt}"
-            ssh -F ../work_data/ssh_config ${opt}
-            break
-            ;;
-        "cm1")
-            echo "Connecting to ${opt}"
-            ssh -F ../work_data/ssh_config ${opt}
-            break
-            ;;
-        "agent_1")
-            echo "Connecting to ${opt}"
-            ssh -F ../work_data/ssh_config ${opt}
-            break
-            ;;
-        "docker_agent")
-            echo "Connecting to ${opt}"
-            ssh -F ../work_data/ssh_config ${opt}
-            break
-            ;;
-        "Quit")
-            break
-            ;;
-        *) echo "invalid option $REPLY";;
+        *) echo "invalid option";;
     esac
-done
+
+    exit
+fi
+
+if [ $# -eq 0 ]
+then
+    PS3='Please enter your choice: '
+    options=("bastion" "cjoc" "cm1" "agent_1" "docker_agent" "Quit")
+    select opt in "${options[@]}"
+    do
+        case $opt in
+            "bastion")
+                echo "Connecting to ${opt}"
+                ssh -F ../work_data/ssh_config ${opt}
+                break
+                ;;
+            "cjoc")
+                echo "Connecting to ${opt}"
+                ssh -F ../work_data/ssh_config ${opt}
+                break
+                ;;
+            "cm1")
+                echo "Connecting to ${opt}"
+                ssh -F ../work_data/ssh_config ${opt}
+                break
+                ;;
+            "agent_1")
+                echo "Connecting to ${opt}"
+                ssh -F ../work_data/ssh_config ${opt}
+                break
+                ;;
+            "docker_agent")
+                echo "Connecting to ${opt}"
+                ssh -F ../work_data/ssh_config ${opt}
+                break
+                ;;
+            "Quit")
+                break
+                ;;
+            *) echo "invalid option $REPLY";;
+        esac
+    done
+fi
