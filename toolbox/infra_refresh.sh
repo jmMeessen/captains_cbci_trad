d16c3bb74e5de1f7a78994d6a8029fa03fa0c40d
Jean-Marc Meessen <jean-marc@meessen-web.org>
2021-05-01 13:12:48 +0200

    Refresh TF state and rebuild ssh_config

--- toolbox/infra_refresh.sh
+++ toolbox/infra_refresh.sh
@@ -13,5 +13,27 @@ source ~/.ovhrc
 ./update_aws_token.sh
 
 cd ../terraform
+echo "Refreshing the Terraform state"
 terraform refresh
-cd -
+
+# delete the hostfile just in case
+rm ../work_data/hostfile
+
+
+echo "\nGathering data from terraform state"
+the_bastion_ip=$(terraform output -raw bastion_ip 2>&1)
+the_cjoc_ip=$(terraform output -raw cjoc_ip 2>&1)
+the_cm1_ip=$(terraform output -raw cm1_ip 2>&1)
+the_docker_agent_ip=$(terraform output -raw docker_agent_ip 2>&1)
+the_agent1_ip=$(terraform output -raw agent1_ip 2>&1)
+
+echo "\nWriting new \"ssh_config\" file."
+#Read in template one line at the time, and replace variables (more
+#natural (and efficient) way, thanks to Jonathan Leffler).
+echo " " > ../work_data/ssh_config
+while read line
+do
+    eval echo "$line" >> ../work_data/ssh_config
+done < "../terraform/ssh-config.tpl"
+
+echo "Done.\n"
