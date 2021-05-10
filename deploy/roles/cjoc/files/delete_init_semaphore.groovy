4361ca8472c024dc46e7dc5b9a23ed5ff9e09909
Jean-Marc Meessen <jean-marc@meessen-web.org>
2021-05-10 22:40:20 +0200

    WIP

--- deploy/roles/cjoc/files/delete_init_semaphore.groovy
+++ deploy/roles/cjoc/files/delete_init_semaphore.groovy
@@ -1,5 +1,7 @@
 import java.util.logging.Logger
 
+//The semaphore is set at the begining of the init groovy
+
 String scriptName = "delete_init_semaphore.groovy"
 Logger logger = Logger.getLogger(scriptName)
 logger.info("---> Starting ${scriptName}")
@@ -13,3 +15,7 @@ String filename = jenkins_home + "/init_semaphore.txt"
 boolean fileSuccessfullyDeleted =  new File(filename).delete()  
 println "File deleted: "+ fileSuccessfullyDeleted
 logger.info("     delete result: " + fileSuccessfullyDeleted)
+
+// def firstInitDone_File = new File(jenkins_home + "/firstInitDone.txt")
+// firstInitDone_File.createNewFile()
+// firstInitDone_File.write("first init done.") 
